e556cd6cdf5adc2839e78b1c43ba71203ea1d9d5
salmaarayyan <salmaarayyan@gmail.com>
2024-03-25 14:22:05 +0700

    membuat prosedur input

---
+++ calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+void input()
+{
+    cout << "masukkan bilangan pertama = ";
+    cin >> bilSatu;
+    cout << "masukkan bilangan kedua = ";
+    cin >> bilDua;
+}
