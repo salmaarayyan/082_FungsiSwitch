54fa5a0914ecccd397e46b9bf3c43bc95b6dfaf7
salmaarayyan <salmaarayyan@gmail.com>
2024-03-25 14:33:08 +0700

    menampilkan pengurangan

--- calculator.cpp
+++ calculator.cpp
@@ -41,6 +41,7 @@ int main()
         cout << "1. penjumlahan " << endl;
         cout << "2. pembagian " << endl;
         cout << "3. perkalian " << endl;
+        cout << "4. pengurangan " << endl;
         
         
     }
